b4ba90c10a5bd01bec122e6f85694561d68b034d
afrin-titly <titlee42@gmail.com>
2024-12-18 22:58:36 +0900

    main function

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module book-apis
 go 1.23.1
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"book-apis/application"
+	"book-apis/infrastucture"
+	"book-apis/interfaces"
+	"database/sql"
+	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func routes(h *interfaces.BookHandler) *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/books", h.GetAllBookHandler).Methods("GET")
+	return r
+}
+
+func main() {
+	connStirng := "host=localhost port=3306 user=mysql password=secret dbname=books sslmode=disable"
+	db, err := sql.Open("mysql", connStirng)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	repo := infrastucture.NewBookRepositoryDB(db)
+	service := application.NewBookService(repo)
+	handler := interfaces.NewBookHandler(service)
+	r := routes(handler)
+	http.ListenAndServe(":8080", r)
 }
