a1c7d7ead8126751411b9b555adb3800631fa7a9
afrin-titly <titlee42@gmail.com>
2024-12-18 22:17:58 +0900

    implement http handler

--- interfaces/book_handler.go
+++ interfaces/book_handler.go
@@ -1,1 +1,25 @@
 package interfaces
+
+import (
+	"book-apis/application"
+	"encoding/json"
+	"net/http"
+)
+
+type BookHandler struct {
+	service *application.BookService
+}
+
+func NewBookHandler(service *application.BookService) *BookHandler {
+	return &BookHandler{service: service}
+}
+
+func (s *BookHandler) GetAllBookHandler(w http.ResponseWriter, r *http.Request) {
+	books, err := s.service.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
--- interfaces/book_handler_test.go
+++ interfaces/book_handler_test.go
@@ -1,40 +1,77 @@
 package interfaces_test
 
 import (
+	"book-apis/application"
 	"book-apis/domain"
+	"book-apis/interfaces"
 	"book-apis/mocks"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
-func TestBookHandler_GetAll(t *testing.T) {
+func TestGetAllBooks(t *testing.T) {
+	repo := &mocks.MockBookRepository{}
+	service := application.NewBookService(repo)
+	h := interfaces.NewBookHandler(service)
+
 	type testCase struct {
-		name      string
-		expected  []domain.Book
-		mockSetup func(*mocks.MockBookRepository)
+		name       string
+		expected   []domain.Book
+		mockSetup  func()
+		statusCode int
 	}
 
 	tests := []testCase{
 		{
-			name: "Successfull request",
+			name: "Successful response",
 			expected: []domain.Book{
-				{ID: 1, Title: "Test Title 1", Author: "Test Author 1"},
-				{ID: 2, Title: "Test Title 2", Author: "Test Author 2"},
+				{ID: 1, Title: "Book 1", Author: "Author"},
+				{ID: 2, Title: "Book 2", Author: "Author"},
 			},
-			mockSetup: func(mockRepo *mocks.MockBookRepository) {
-				mockRepo.On("GetAll", []domain.Book{
-					{ID: 1, Title: "Book 1", Author: "Author 1"},
-					{ID: 2, Title: "Book 2", Author: "Author 2"},
-				}).Return([]domain.Book{
-					{ID: 1, Title: "Book 1", Author: "Author 1"},
-					{ID: 2, Title: "Book 2", Author: "Author 2"},
-				}, nil)
+			mockSetup: func() {
+				repo.On("GetAll").Return([]domain.Book{
+					{ID: 1, Title: "Book 1", Author: "Author"},
+					{ID: 2, Title: "Book 2", Author: "Author"},
+				}, nil).Once()
 			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name:     "UnSuccessful response",
+			expected: nil,
+			mockSetup: func() {
+				repo.On("GetAll").Return([]domain.Book(nil), errors.New("Some error message")).Once()
+			},
+			statusCode: http.StatusInternalServerError,
 		},
 	}
+
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			tc.mockSetup()
+			req, err := http.NewRequest("GET", "/books", nil)
+			if err != nil {
+				t.Fatalf("Failed to create request: %v", err)
+			}
 
+			response := httptest.NewRecorder()
+			handler := http.HandlerFunc(h.GetAllBookHandler)
+			handler.ServeHTTP(response, req)
+
+			if response.Code != tc.statusCode {
+				t.Errorf("Expected status code %d, but got %d", tc.statusCode, response.Code)
+			}
+			var books []domain.Book
+			json.NewDecoder(response.Body).Decode(&books)
+
+			if !reflect.DeepEqual(books, tc.expected) {
+				t.Errorf("Handler returned unexpected body:\nGot:  %+v\nWant: %+v", books, tc.expected)
+			}
 		})
 	}
+
 }
