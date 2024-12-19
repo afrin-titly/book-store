d0f24d8953b5e7ac07c21f4ac16c139be76a2975
afrin-titly <titlee42@gmail.com>
2024-12-19 23:03:01 +0900

    fix test

--- application/book_service_test.go
+++ application/book_service_test.go
@@ -3,49 +3,67 @@ package application_test
 import (
 	"book-apis/application"
 	"book-apis/domain"
+	"book-apis/mocks"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type MockRepository struct {
-	Books []domain.Book
-	Err   error
-}
-
-func (m *MockRepository) GetAll() ([]domain.Book, error) {
-	return m.Books, m.Err
-}
-
 func TestBookService_GetAll(t *testing.T) {
 	type testCase struct {
 		name      string
 		expected  []domain.Book
-		mockSetup *MockRepository
+		mockSetup func(mockRepo *mocks.MockBookRepository)
 	}
 	tests := []testCase{
 		{
-			name: "Successfull Retrival",
+			name: "Successful Retrieval",
 			expected: []domain.Book{
 				{Title: "Test Title 1", Author: "Test Author 1", Genre: "Horror", Price: "100", Stock: 10},
 				{Title: "Test Title 2", Author: "Test Author 2", Genre: "Adventure", Price: "150", Stock: 20},
 			},
-			mockSetup: &MockRepository{
-				Books: []domain.Book{
+			mockSetup: func(mockRepo *mocks.MockBookRepository) {
+				mockRepo.On("GetAll").Return([]domain.Book{
 					{Title: "Test Title 1", Author: "Test Author 1", Genre: "Horror", Price: "100", Stock: 10},
 					{Title: "Test Title 2", Author: "Test Author 2", Genre: "Adventure", Price: "150", Stock: 20},
-				},
-				Err: nil,
+				}, nil)
+			},
+		},
+		{
+			name:     "Empty List",
+			expected: []domain.Book{},
+			mockSetup: func(mockRepo *mocks.MockBookRepository) {
+				mockRepo.On("GetAll").Return([]domain.Book{}, nil)
+			},
+		},
+		{
+			name:     "Error Retrieval",
+			expected: nil,
+			mockSetup: func(mockRepo *mocks.MockBookRepository) {
+				mockRepo.On("GetAll").Return([]domain.Book(nil), assert.AnError)
 			},
 		},
 	}
+
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			service := application.NewBookService(tc.mockSetup)
+			mockRepo := new(mocks.MockBookRepository)
+
+			tc.mockSetup(mockRepo)
+
+			service := application.NewBookService(mockRepo)
+
 			result, err := service.GetAll()
 
-			assert.NoError(t, err)
-			assert.Equal(t, len(tc.expected), len(result))
+			if tc.expected != nil {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, result)
+			} else {
+				assert.Error(t, err)
+				assert.Nil(t, result)
+			}
+
+			mockRepo.AssertExpectations(t)
 		})
 	}
 }
